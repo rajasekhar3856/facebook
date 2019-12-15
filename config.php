fe9ed901e10e8551ec8b040b9a33e05632d2eda1
rajasekhar3856 <38796757+rajasekhar3856@users.noreply.github.com>
2019-12-15 12:04:38 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Basic Site Settings and API Configuration
+ */
+
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'fb');
+define('DB_USER_TBL', 'users');
+
+// Facebook API configuration
+define('FB_APP_ID', '3201382433212545');
+define('FB_APP_SECRET', '8ae75ba68686d55cca1a746fcff27d5a');
+define('FB_REDIRECT_URL', 'http://localhost/fblog/');
+
+// Start session
+if(!session_id()){
+    session_start();
+}
+
+// Include the autoloader provided in the SDK
+require_once __DIR__ . '/facebook-php-graph-sdk/autoload.php';
+
+// Include required libraries
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+
+// Call Facebook API
+$fb = new Facebook(array(
+    'app_id' => FB_APP_ID,
+    'app_secret' => FB_APP_SECRET,
+    'default_graph_version' => 'v3.2',
+));
+
+// Get redirect login helper
+$helper = $fb->getRedirectLoginHelper();
+
+// Try to get access token
+try {
+    if(isset($_SESSION['facebook_access_token'])){
+        $accessToken = $_SESSION['facebook_access_token'];
+    }else{
+          $accessToken = $helper->getAccessToken();
+    }
+} catch(FacebookResponseException $e) {
+     echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+} catch(FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
+}
